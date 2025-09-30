c5d0a01eeec63184a5e5b74f290bc4247cbe3039
Alex <alex_mcgarry@hotmail.co.uk>
2025-09-30 15:27:25 +0100

    update metadata

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -6,7 +6,7 @@ export const fontSans = Inter({
   weight: ["400", "500", "600", "700"],
   variable: "--font-sans",
   display: "swap",
-  preload: false,
+  preload: true,
 });
 
 export const fontBody = fontSans;
@@ -16,5 +16,5 @@ export const fontHeading = Poppins({
   weight: ["400", "500", "600", "700"],
   variable: "--font-display",
   display: "swap",
-  preload: false,
+  preload: true,
 });
